5e71e70a343be7d33b14098b616febf6ad62974a
xy___ok <xy___ok@163.com>
2015-03-20 14:57:29 +0800

    进度条实现

---
+++ progressbar.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python
+#encoding:utf-8
+'''进度条实现'''
+import sys
+import time
+for x in xrange(0,51):
+    sys.stdout.write("Computing: [%s%s] %i%%\r" % ('/' * x , '-' * (50 - x) , x *2))
+    sys.stdout.flush()
+    time.sleep(0.02)
